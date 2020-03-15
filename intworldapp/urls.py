388fa4abc910b50a3be050251720bc53aeefde24
integerJI <jjs9536@gmail.com>
2020-03-15 21:28:31 +0900

    로그인 롤백

--- intworldapp/urls.py
+++ intworldapp/urls.py
@@ -1,7 +1,8 @@
+# myApp/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name="home"),
+    path('index/', views.index, name="index"),
 ]
-
--- intworlduser/views.py
+++ intworlduser/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 # Create your views here.
 
-
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
@@ -12,10 +11,6 @@ def signup(request):
         return redirect('signin')
     return render(request, 'signup.html')
 
-
-def signin(request):
-    return render(request, 'signin.html')
-    
 def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -23,9 +18,9 @@ def signin(request):
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            return redirect('index')
     else:
         return render(request, 'signin.html')
 
 def signout(request):
-    return render(request, 'signout.html')
+    return render(request, 'signin.html')
