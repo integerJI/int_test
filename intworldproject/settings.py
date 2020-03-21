b5b8a6b798dfffdd771e401587197f422e62d63b
integerJI <jjs9536@gmail.com>
2020-03-22 00:53:22 +0900

    회원가입 간략화

--- intworldproject/settings.py
+++ intworldproject/settings.py
@@ -129,6 +129,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
+LOGIN_URL = '/signin/'
+
 LOGIN_REDIRECT_URL = 'index'
 
-LOGOUT_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'signin'
--- intworlduser/views.py
+++ intworlduser/views.py
@@ -1,35 +1,20 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
-from .forms import UserCreationMultiForm
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
-from django.contrib import auth
-# Create your views here.
+from django.conf import settings
 
 def signup(request):
     if request.method == 'POST':
-        if request.POST['user-password1'] == request.POST['user-password2']:
-            form = UserCreationMultiForm(request.POST, request.FILES)
-            if form.is_valid(): 
-                user = form['user'].save()
-                profile = form['profile'].save(commit=False)
-                profile.user = user
-                profile.nick = user
-                profile.save()
-                return redirect('login')
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            return redirect('signin')
     return render(request, 'signup.html')
 
-def signin(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('index')
-    else:
-        return render(request, 'signin.html')
+class Loginviews(LoginView):
+    template_name = 'signin.html'
+signin = Loginviews.as_view()
 
-def signout(request):
-    return render(request, 'signin.html')
+
+class LogoutViews(LogoutView):
+    next_page = settings.LOGOUT_REDIRECT_URL
+signout = LogoutViews.as_view()
