a7ef2ff990e032bc9b459586eaa0a6c0fd973d65
integerJI <jjs9536@gmail.com>
2020-03-22 22:03:57 +0900

    글 불러오기 생성

--- intworldapp/models.py
+++ intworldapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    main_text = models.CharField(max_length=200)
+    create_date = models.DateTimeField('date published')
+    create_user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.main_text
