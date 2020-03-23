089bfa82214bdc51decc3a7b4a77512718827dae
integerJI <jjs9536@gmail.com>
2020-03-23 22:41:56 +0900

    redirect에 reverse 추가로 해결, 정렬 순서 변경

--- intworldapp/views.py
+++ intworldapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import Post
 
 def index(request):
-    posts = Post.objects
+    posts = Post.objects.order_by('-id')
     return render(request, 'index.html', {'posts':posts})
 
 def post(request):
@@ -14,5 +14,5 @@ def post(request):
         post.create_user = User.objects.get(username = request.user.get_username())
         post.create_date = timezone.datetime.now()
         post.save()
-        return redirect('/index/')
+        return redirect(reverse('index'))
     return render(request, 'post.html')
