e1b7ac1b635a1d4c33d5e1743fa7b5ed16dc4df5
integerJI <jjs9536@gmail.com>
2020-04-03 23:18:47 +0900

    길을 잃었다

--- intworldproject/settings.py
+++ intworldproject/settings.py
@@ -123,8 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'intworldproject', 'static')
+    os.path.join(BASE_DIR, 'intworldapp', 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
