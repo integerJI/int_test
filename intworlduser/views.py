321657e2ab3b68bf5761e5d4e19b1ed4820529d8
integerJI <jjs9536@gmail.com>
2020-04-11 10:31:58 +0900

    마이페이지, 사용자페이지 추가, 수정중

--- intworlduser/views.py
+++ intworlduser/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from .forms import UserCreationMultiForm, ProfileForm, ProfileUpdateForm
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
     if request.method == 'POST':
@@ -34,3 +35,42 @@ signin = Loginviews.as_view()
 class LogoutViews(LogoutView):
     next_page = settings.LOGOUT_REDIRECT_URL
 signout = LogoutViews.as_view()
+
+
+@login_required
+def userinfo(request):
+    conn_user = request.user
+    conn_profile = Profile.objects.get(user=conn_user)
+
+    if not conn_profile.profile_image:
+        pic_url = ""
+    else:
+        pic_url = conn_profile.profile_image.url
+            
+    context = {
+        'id' : conn_user.username,
+        'nick' : conn_profile.nick,
+        'profile_pic' : pic_url,
+        'intro' : conn_profile.intro,
+    }
+
+    return render(request, 'mypage.html', context=context)
+
+@login_required
+def user_select_info(request, writer):
+    select_profile = Profile.objects.get(id=writer)
+    select_user = select_profile.user
+
+    if not select_profile.profile_image:
+        pic_url = ""
+    else:
+        pic_url = select_profile.profile_image.url
+            
+    context = {
+        'id' : select_user.username,
+        'nick' : select_profile.nick,
+        'profile_pic' : pic_url,
+        'intro' : select_profile.intro
+    }
+
+    return render(request, 'userpage.html', context=context)
