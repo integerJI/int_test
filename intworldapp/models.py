a75dea48caf9a52d2b39cca83d5df19ffa695df3
integerJI <jjs9536@gmail.com>
2020-04-15 19:45:07 +0900

    글쓰기 form으로 바꿈, 해시태그 추가중

--- intworldapp/models.py
+++ intworldapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+import re
 
 class Post(models.Model):
     main_text = models.CharField(max_length=200)
@@ -9,6 +10,7 @@ class Post(models.Model):
     update_date = models.DateTimeField('date published', null = True, default=timezone.now)
     create_img = models.ImageField(blank=True, upload_to="post/%Y/%m/%d", null=True)
     likes = models.ManyToManyField(User, related_name='likes')
+    tag_set = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.main_text
@@ -16,6 +18,18 @@ class Post(models.Model):
     @property
     def total_likes(self):
         return self.likes.count()
+
+    # NOTE: content에서 tags를 추출하여, Tag 객체 가져오기, 신규 태그는 Tag instance 생성, 본인의 tag_set에 등록,
+    def tag_save(self):
+        tags = re.findall(r'#(\w+)\b', self.main_text)
+
+        if not tags:
+            return
+
+        for t in tags:
+            tag, tag_created = Tag.objects.get_or_create(tag_name=t)
+            self.tag_set.add(tag)  # NOTE: ManyToManyField 에 인스턴스 추가
+
         
 class Comment(models.Model):
     comment = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name='comments')
@@ -28,3 +42,9 @@ class Comment(models.Model):
             
     def __str__(self):
         return '%s - %s' % (self.comment_user, self.comment_text) 
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=140, unique=True)
+
+    def __str__(self):
+        return self.tag_name
--- intworldapp/views.py
+++ intworldapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Post, Comment
+from .models import Post, Comment, Tag
+from .forms import PostForm
 from intworlduser.models import Profile
 
 try:
@@ -38,14 +39,18 @@ def dev(request):
     return render(request, 'devpage.html')
 
 def post(request):
-    if request.method == 'POST':
-        post = Post()
-        post.main_text = request.POST['main_text']
-        post.create_img = request.FILES['create_img']
-        post.create_user = User.objects.get(username = request.user.get_username())
-        post.save()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        
+        if form.is_valid(): 
+            post = form.save(commit = False)
+            post.create_user = User.objects.get(username = request.user.get_username())
+            post.tag_save()
+            post.save()
         return redirect(reverse('index'))
-    return render(request, 'post.html')
+    else:
+        form = PostForm() 
+        return render(request, 'post.html', {'form' : form})  
 
 def detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
