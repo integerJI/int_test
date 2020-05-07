2803679b6cd6c9db3000390f88e3f6f7f7775e66
integerJI <jjs9536@gmail.com>
2020-05-07 23:14:16 +0900

    검색 역순 수정

--- intworldapp/views.py
+++ intworldapp/views.py
@@ -161,12 +161,12 @@ def search(request, tag=None, tag_2=None):
     q = request.POST.get('q', False) 
 
     if tag:
-        posts = Post.objects.filter(tag_set__tag_name__iexact=tag).prefetch_related('tag_set').select_related('create_user')
+        posts = Post.objects.filter(tag_set__tag_name__iexact=tag).prefetch_related('tag_set').select_related('create_user').order_by('-id')
         if tag_2:
-            posts = Post.objects.filter(tag_set_2__tag_name_2__iexact=tag_2).prefetch_related('tag_set_2').select_related('create_user')
+            posts = Post.objects.filter(tag_set_2__tag_name_2__iexact=tag_2).prefetch_related('tag_set_2').select_related('create_user').order_by('-id')
 
     elif q:
-        posts = posts.filter(main_text__icontains=q)
+        posts = posts.filter(main_text__icontains=q).order_by('-id')
     
     else:
         messages.info(request, '입력된 값이 없습니다.')
